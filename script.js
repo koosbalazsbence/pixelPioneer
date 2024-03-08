a32dc3159f5b5240dd0f0f94ad6c7023ab6d432d
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-03-08 19:18:29 +0100

    updateCommit

--- script.js
+++ script.js
@@ -48,3 +48,14 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #737373}"
     document.body.appendChild(css)
 }
+
+let mybutton = document.getElementById("myBtn")
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+  document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? mybutton.style.display = "block" : mybutton.style.display = "none"
+}
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
